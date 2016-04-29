73cf72549468a22103363201329672d58c55068f
Jiang haiyun <jiang.haiyun@gmail.com>
2016-04-29 11:14:30 +0800

    bit operator

---
+++ c-lang-v2/2-6.setbits.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n bits
+that begin at position p set to the rightmost n bits of y, leaving the other bits
+unchanged.
+*/
+
+long setbits(long x, int p, int n, int y)
+{
+    int mask;
+    if ( y == 0 ) {
+        mask = (~0 << (p+1) ) | ~(~0 << (p+1-n)); /* 1111111100000111*/
+        return x & mask;
+    }
+    else {
+        mask = ~(~0 << (p+1)) & (~0 << (p+1)-n); /* 0000000000001111110000*/
+        return x | mask;
+    } 
+ }
+
+int main()
+{
+    if ( setbits( 0xFFFF, 3, 3, 0) == 0xFFF1 ) {
+        printf("set to 0 ok\n");
+    }
+    if ( setbits( 0xFFF1, 3, 3, 1 ) == 0xFFFF ) {
+        printf("set to 1 ok\n");
+    }
+}
---
+++ c-lang-v2/2-7.invert.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that
+begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the
+others unchanged.
+*/
+
+long invert(long x, int p, int n)
+{
+    int mask;
+    mask = ~(~0 << (p+1)) & (~0 << (p+1)-n); /* 0000000000001111110000*/
+    return x ^ mask;
+ }
+
+int main()
+{
+    if ( invert( 0xFFFF, 3, 3) == 0xFFF1 ) {
+        printf("invert1 passed!\n");
+    }
+    if ( invert( 0xFFF1, 3, 3) == 0xFFFF ) {
+        printf("invert2 passed!\n");
+    }
+}
