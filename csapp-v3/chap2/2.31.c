bd077923b8bf64bab89ece06b7f2777b7d5e80e6
haiiiiiyun <jiang.haiyun@gmail.com>
2016-05-19 22:08:32 +0800

    csapp --> 2.31

--- csapp-v3/chap2/2.31.c
+++ csapp-v3/chap2/2.31.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-/* p94, 2.30: */
+/* p94, 2.31: */
 /* Determine whether arguments can be added without overflow */
-/* 二进制补码表示的有符号数相加溢出的判断为：
- * ( x>0 && y>0 && x+y < 0 ) || (x<0 && y<0 && x+y>0)
- */
+/* WARNING: This code is buggy. */
+
+/*　(sum-x==y) && (sum-y==x)是永远成立的，
+ *  例如，对于 sum-x, 不管sum溢出与否，值都会最终等于y
+ *  例： x= Tmin=-pow(2,w-1), y= -1, 此时x+y会溢出。
+ *  sum = x+y　溢出后最终值为　-pow(2,w-1)-1+pow(2,w)=pow(2,w-1)-1
+ *  对于(sum-x==y)的判断：
+ *  sum-x=pow(2,w-1)-1-(-pow(2,w-1))=pow(2,w)-1, 溢出后最终sum=pow(2,w)-1-pow(2,w)=-1, 即(sum-x==y) 类似 (sum-y==x)也永远成立*/
 
 int tadd_ok( int x , int y )
 {
-    int s = x + y;
+    int sum = x + y;
 
-    if ( ( x>0 && y>0 && s<0 ) || ( x<0 && y<0 && s>0 ) )
-        return 0;
-    return 1;
+    return (sum-x==y) && (sum-y == x);
 }
 
 int main(void)
 {
 	int x,y, s;
 
+    /* 以下tadd_ok调用全部都返回1 */
 	/* case1: negetive overflow */
 	x = -pow(2, sizeof(int)*8-1); /* Tmin */
 	y = -1;
