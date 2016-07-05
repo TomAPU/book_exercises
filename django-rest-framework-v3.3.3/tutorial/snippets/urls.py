4fb820982a84475634a6ada3b4f8234a0dd445b1
haiiiiiyun <jiang.haiyun@gmail.com>
2016-07-05 16:50:11 +0800

    finish tutorial 4, authentication and permission

--- django-rest-framework-v3.3.3/tutorial/snippets/urls.py
+++ django-rest-framework-v3.3.3/tutorial/snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
