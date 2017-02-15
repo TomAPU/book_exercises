261f5332a375138a3c89a5a5aa38b26a5f748c27
Jiang Haiyun <jiang.haiyun@gmail.com>
2017-02-15 22:02:37 +0800

    使用通用视图重构

--- django-rest-framework-v3/tutorial/tutorial/snippets/views.py
+++ django-rest-framework-v3/tutorial/tutorial/snippets/views.py
@@ -1,43 +1,22 @@
-from rest_framework import mixins
 from rest_framework import generics
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 # A view that supports listing all the existing snippets, or creating a new snippet.
-class SnippetList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new one.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
 # A view that corresponds to an individual snippet, can be used to
 # retrieve, update or delete the snippet.
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
