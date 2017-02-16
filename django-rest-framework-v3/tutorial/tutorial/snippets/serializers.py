bad121ca134a298e9ed03a842ba9679c975dec72
Jiang haiyun <jiang.haiyun@gmail.com>
2017-02-16 16:13:11 +0800

    Hyperlinked relationship

--- django-rest-framework-v3/tutorial/tutorial/snippets/serializers.py
+++ django-rest-framework-v3/tutorial/tutorial/snippets/serializers.py
@@ -4,11 +4,11 @@ from .models import Snippet
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-hightlight', format='html')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('url', 'id', 'hightlight', 'owner',
+        fields = ('url', 'id', 'highlight', 'owner',
                 'title', 'code', 'linenos', 'language', 'style',)
 
 
