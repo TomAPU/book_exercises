6dbc63b66e8785eb55991161955714c8cff043a2
Jiang haiyun <jiang.haiyun@gmail.com>
2017-02-16 10:57:59 +0800

    remove rest pagination

--- django-rest-framework-v3/tutorial/tutorial/settings.py
+++ django-rest-framework-v3/tutorial/tutorial/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASS': [
         'rest_framework.permissions.IsAdminUser', # 只允许管理员访问 API
     ],
-    'PAGE_SIZE': 10 # 开启分页
+    #'PAGE_SIZE': 10 # 开启分页
 }
 
 MIDDLEWARE = [
