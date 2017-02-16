924a607ffc96d10912dab2082eab38b845894bd9
Jiang haiyun <jiang.haiyun@gmail.com>
2017-02-16 10:23:42 +0800

    add authentication and permissions

--- django-rest-framework-v3/tutorial/tutorial/snippets/views.py
+++ django-rest-framework-v3/tutorial/tutorial/snippets/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
+from rest_framework import permissions
+
+from django.contrib.auth.models import User
 
 from .models import Snippet
 from .serializers import SnippetSerializer
+from .serializers import UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # A view that supports listing all the existing snippets, or creating a new snippet.
 class SnippetList(generics.ListCreateAPIView):
@@ -11,6 +16,11 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 # A view that corresponds to an individual snippet, can be used to
 # retrieve, update or delete the snippet.
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -18,5 +28,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     Retrieve, update or delete a code snippet.
     """
 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
