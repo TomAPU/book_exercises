b14e001cb534e3d5d8c0a631e93850cb2ac917f2
Jiang haiyun <jiang.haiyun@gmail.com>
2017-02-17 10:39:42 +0800

    用 Router 重构

--- django-rest-framework-v3/tutorial/tutorial/snippets/views.py
+++ django-rest-framework-v3/tutorial/tutorial/snippets/views.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 from rest_framework import renderers
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -41,10 +40,3 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-@api_view(['GET',])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
