221a163027cbb0cc1720103ef697e49e8882b839
Jiang haiyun <jiang.haiyun@gmail.com>
2017-02-21 15:34:44 +0800

    not working

---
+++ ExtJS6ByExample/chap7/expenseanalyzer/build/temp/development/EA/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/home/hy/workspace/book_exercises/ExtJS6ByExample/chap7/expenseanalyzer/build/.sass-cache'
---
+++ ExtJS6ByExample/chap7/expenseanalyzer/build/temp/development/EA/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /home/hy/workspace/book_exercises/ExtJS6ByExample/chap7/expenseanalyzer/app.js
