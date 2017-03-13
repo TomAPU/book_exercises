dbf25557c5f961ed7811a946599b17ac95537368
Jiang haiyun <jiang.haiyun@gmail.com>
2017-03-13 14:28:52 +0800

    显示单个 Article

--- ng-book2/writingYourFirstAngular2WebApp/angular2-reddit/src/app/app.component.ts
+++ ng-book2/writingYourFirstAngular2WebApp/angular2-reddit/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
     // functionName(propertyName1: typeName, propertyName2: typeName): returnTypeName
     // 这些参数需要在响应事件时传入
     addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+        // 这里又用 ` 来包围字符串，在 ES6 中，这种字符串能扩展里面的变量值
         console.log(`Adding article title: ${title.value} and link: ${link.value}`);
         return false;
     }
---
+++ ng-book2/writingYourFirstAngular2WebApp/angular2-reddit/src/app/article/article.component.ts
@@ -0,0 +1,43 @@
+/*
+ * host 就是代表当前组件关联到的元素。这里设置将
+ * 每个 app-article 标签上都加个 'row' 类。通过 host，
+ * 我们可以在组件内对元素标签进行配置
+ */
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model.ts';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+  host: {
+      class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  // 这些属性可以在组件模板中引用
+  @Input() article: Article;
+
+  constructor() {
+      this.article = new Article(
+          'Angular 2',
+          'http://angular.io',
+          10
+      );
+  }
+
+  // 这些函数可以在组件模板中引用
+  voteUp(): boolean {
+      this.article.voteUp();
+      return false;
+  }
+
+  voteDown(): boolean {
+      this.article.voteDown();
+      return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
