da56999199c28a3bd0825d01f97a9fcffd35a2d7
Jiang Haiyun <jiang.haiyun@gmail.com>
2017-03-20 22:49:54 +0800

    ng book2 DI

--- ng-book2/dependency_injection/injector/app/ts/app.ts
+++ ng-book2/dependency_injection/injector/app/ts/app.ts
@@ -14,15 +14,14 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
  */
 require('css/styles.css');
 
-/*
- * The injectable service
- */
+// 先创建一个只返回一个字符串的服务：
 class MyService {
   getValue(): string {
     return 'a value';
   }
 }
 
+// 应用组件
 @Component({
   selector: 'di-sample-app',
   template: `
@@ -33,8 +32,17 @@ class DiSampleApp {
   myService: MyService;
 
   constructor() {
+      // 使用静态方法 ReflectiveInjector.resolveAndCreate 来创建 Injector 实例
+      // 传入的参数是一个数组，包含所有该 Injector 实例需知道的
+      // 可注入对象，这里我们只想让它知道
+      // MyService 这个可注入对象
+      // ReflectiveInjector 是 Injector 的一个具体实现
+      // 它是最常的 Injector 类
     let injector: any = ReflectiveInjector.resolveAndCreate([MyService]);
     this.myService = injector.get(MyService);
+
+      // 需要注意的是：注入的是一个单例实例
+      // 因此，这里输出是 "Same instance? true"
     console.log('Same instance?', this.myService === injector.get(MyService));
   }
 
@@ -43,6 +51,8 @@ class DiSampleApp {
   }
 }
 
+// 由于使用了自己的 Injector 实现，因此无需和以前那样
+// 将 MyService 放入 NgModule 的 providers 列表中
 // no need to add injectables here
 @NgModule({
   declarations: [ DiSampleApp ],
